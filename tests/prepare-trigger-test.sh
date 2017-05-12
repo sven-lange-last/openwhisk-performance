163ff26451d6b386392cb35f0d6a7c3d590fbfd6
Sven Lange-Last <sven.lange-last@de.ibm.com>
2017-05-12 15:10:02 +0200

    Add test for firing no-op trigger w/o rule

---
+++ tests/prepare-trigger-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Host to use. Needs to include the protocol.
+host=$1
+# Credentials to use for the test. USER:PASS format.
+credentials=$2
+# Name of the trigger to create and test.
+trigger=$3
+
+# create a noop trigger
+echo "Creating trigger $trigger"
+curl -u "$credentials" "$host/api/v1/namespaces/_/triggers/$trigger" -XPUT -d '{"name":"$trigger"}' -H "Content-Type: application/json"
+
+# fire the noop trigger
+echo "Running $trigger once to assert an intact system"
+curl -u "$credentials" "$host/api/v1/namespaces/_/triggers/$trigger" -XPOST
---
+++ tests/trigger-test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+currentDir="$(dirname "$0")"
+
+# Host to use. Needs to include the protocol.
+host=$1
+# Credentials to use for the test. USER:PASS format.
+credentials=$2
+# concurrency level of the throughput test: How many requests should
+# open in parallel.
+concurrency=$3
+# How many samples to create by the test. Default: 10000
+samples=${4:-10000}
+
+trigger="trigger-test"
+"$currentDir/create.sh" "$host" "$credentials" "$trigger"
+
+# run throughput tests
+encodedAuth=$(echo "$credentials" | base64 -w 0)
+docker run --rm markusthoemmes/loadtest loadtest -n "$samples" -c "$concurrency" -k -m POST -H "Authorization: basic $encodedAuth" "$host/api/v1/namespaces/_/triggers/$trigger"
